61e8f077364443fcf897029709ca181880bbff28
its-a-setup <96050384+its-a-setup@users.noreply.github.com>
2023-06-01 21:52:06 +0800

    add registration

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Productivity WebApp for tracking time towards a goal.
 This app revolves around concept that it takes 10,000 hours to become a PRO in any domain.
+
+The app allows to track time, assign tags to your learning sessions and see the progress towards your goal.
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-from datetime import datetime
-import os
 
 load_dotenv()
 USERNAME = os.getenv("USERNAME")
@@ -14,7 +15,7 @@ app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@localhost:3306/road_10k_v2"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 db = SQLAlchemy(app)
 
@@ -25,6 +26,11 @@ class SESSIONS(db.Model):
     created_at = db.Column(db.DATETIME, default=datetime.utcnow())
     tag = db.Column(db.String(100))
     time_passed = db.Column(db.Time, default="00:00:00")
+
+
+class USERS(db.Model):
+    email = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100))
 
 
 db.create_all()
@@ -39,7 +45,7 @@ def home():
 @app.post("/add")
 def add():
     tag = request.form.get("tag")
-    new_session = SESSIONS(tag=tag, complete=False)
+    new_session = SESSIONS(tag=tag)
     db.session.add(new_session)
     db.session.commit()
     return redirect(url_for("home"))
@@ -47,7 +53,28 @@ def add():
 
 @app.get("/delete/<int:session_id>")
 def delete(session_id):
-    session = db.session.query(SESSIONS).filter(SESSIONS.session_id == session_id).first()
+    session = (
+        db.session.query(SESSIONS).filter(SESSIONS.session_id == session_id).first()
+    )
     db.session.delete(session)
     db.session.commit()
     return redirect(url_for("home"))
+
+
+@app.route("/registration")
+def log_in():
+    return render_template("registration.html")
+
+
+@app.post("/add_user")
+def add_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    new_user = USERS(email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
